7fd1daae7ea80e3e8283e3553b508fed2af33d31
Harvey Rogers <harveyr@gmail.com>
2019-11-15 14:56:26 -0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import * as core from '@actions/core'
+import { wait } from './wait'
+
+async function run() {
+  try {
+    const ms = core.getInput('milliseconds')
+    console.log(`Waiting ${ms} milliseconds ...`)
+
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
+
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run().catch(err => {
+  core.setFailed(`${err}`)
+})
